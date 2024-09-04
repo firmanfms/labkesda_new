4d753d277a1a4fc46461b51b796053867c60bd8d
illusi03 <mohammad.azhari12@gmail.com>
2024-09-04 19:16:05 +0700

    Export lab lingkungan with matrix parameter

--- application/modules/report/controllers/Lab_lingkungan.php
+++ application/modules/report/controllers/Lab_lingkungan.php
@@ -82,11 +82,41 @@ class Lab_lingkungan extends MY_Controller {
                                                 WHERE a.`kd_lab`='".$this->input->post('kd_lab')."' AND a.status='1' 
                                                 AND a.tgl_input BETWEEN '".$tanggal_dari." 00:00:00' 
                                                 AND '".$tanggal_smp." 23:00:00'")->result();
-                                            //    PRE($data);
-                                            //    pre( ($data['pendaftaran']));
-                                            //  echo $this->db->last_query();
-            // test($data['pendaftaran'],1);
-            // $data['data_mutasi']    = $this->mutasi_model->history_stok_barang($id_lokasi,$tanggal_dari,$tanggal_smp,$id_barang);
+			
+            // TODOS: Sesuaikan lagi ke query atas. Query ini untuk Testing saja.
+            // $data['pendaftaran']    = $this->db->query("
+            // SELECT a.nama,a.alamat,a.`no_pendaftaran`,a.`tgl_diterima`,b.nm_sampel,a.kd_lab
+            //     FROM t_pendaftaran a 
+            // LEFT JOIN m_sampel b ON a.`kd_sampel`=b.`kd_sampel`
+            // WHERE a.status='1' LIMIT 100;
+            // ")->result();
+
+
+            // NOTE : Logic untuk Matriks Parameter dan Nilai 
+            $listNoPendaftaran = [];
+            foreach ($data['pendaftaran'] as $key => $value) {
+                $listNoPendaftaran[] = $value->no_pendaftaran;
+            }
+            $detailPendaftaran = [];
+            $resultPendaftaranDetail = $this->db->query("
+                SELECT a.no_pendaftaran, a.kd_parameter, a.nilai
+                    FROM t_pendaftaran_detail a 
+                WHERE a.no_pendaftaran IN ('".implode("','",$listNoPendaftaran)."');
+            ")->result();
+            foreach ($resultPendaftaranDetail as $key => $value) {
+                $detailPendaftaran[$value->no_pendaftaran][$value->kd_parameter] = $value;
+            }
+            foreach ($data['pendaftaran'] as $key => $value) {
+                $data['pendaftaran'][$key]->detail = $detailPendaftaran[$value->no_pendaftaran];
+            }
+            $titleParam = $this->db->query("
+                SELECT DISTINCT(a.kd_parameter), mparam.nm_parameter FROM t_pendaftaran_detail a
+                    INNER JOIN m_parameter mparam ON mparam.kd_parameter = a.kd_parameter
+                WHERE a.no_pendaftaran IN ('".implode("','",$listNoPendaftaran)."');
+            ")->result();
+            $data['title_param'] = $titleParam;
+            // END
+
         }
         $this->load->view('report/lab_lingkungan_excell',$data);
     }
